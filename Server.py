e85edfeae72557fd0ea7d1ed94d8467eb071ab0e
Дмитрий <popkovdima08@mail.ru>
2024-03-20 18:35:06 +0300

    Initial commit

---
+++ Server.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+#4c53675e8cca4ccda784000ffb435032
